f45ce7375e9ae46ae8ed3d0b9617785b0260d8dd
gkdlf9812 <gkdlf9812@gmail.com>
2023-02-14 11:11:39 +0900

    스케너로 명령어 입엽받기

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("=== 프로그렘 시작 ===");
+    
+    Scanner sc = new Scanner(System.in);
+    
+    
+    String cmd = sc.nextLine();
+    cmd = sc.nextLine();
+    
+    System.out.printf("명령어: %s\n", cmd);
+    
+    System.out.println("cmd : " + cmd);
+    
+    sc.close();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    System.out.println("=== 프로그렘 끝 ===");
+    
+    
+    
+  }
+}
