3a390e8086780c5c3daba8a764f6ee23a05ca2cc
gkdlf9812 <gkdlf9812@gmail.com>
2023-02-14 11:48:38 +0900

    무한 루프 반복문 입력

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,24 @@
+
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    System.out.println("=== 프로그렘 시작 ===");
-    
-    Scanner sc = new Scanner(System.in);
-    
-    
-    String cmd = sc.nextLine();
-    cmd = sc.nextLine();
-    
-    System.out.printf("명령어: %s\n", cmd);
-    
-    System.out.println("cmd : " + cmd);
-    
-    sc.close();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    System.out.println("=== 프로그렘 끝 ===");
-    
-    
-    
-  }
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			System.out.printf("명령어 ) ");
+			String command = sc.nextLine();
+
+			if (command.equals("system exit")) {
+				break;
+			}
+		}
+
+		System.out.println("==프로그램 끝==");
+
+		sc.close();
+	}
+
 }
