0e90083117410f512435a8c30ef95de1e01243f5
gkdlf9812 <gkdlf9812@gmail.com>
2023-02-17 11:53:45 +0900

    new commit

---
+++ src/articles/Util.java
@@ -0,0 +1,16 @@
+package articles;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	/** 현재 날짜, 시각 정보 String 리턴 */
+	public static String getNowDateTimeStr() {
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+		Date now = new Date();
+
+		return sdf1.format(now);
+	}
+}
